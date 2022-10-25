1bdb3506ec8fe752748fd0fa3fd88fe2072742c3
Maxime Cerjak <maxpro.cerjak@gmail.com>
2022-10-25 12:10:41 +0200

    change station field

--- src/main/java/com/formation/velo/api/velo/Field.java
+++ src/main/java/com/formation/velo/api/velo/Field.java
@@ -1,6 +1,5 @@
 package com.formation.velo.api.velo;
 
-import com.google.gson.annotations.SerializedName;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,30 +7,13 @@ import lombok.Setter;
 @Getter
 @Setter
 
-@lombok.Data
 public class Field {
-
-    @SerializedName("available_bike_stands")
-    private int availableBikeStands;
-
-    @SerializedName("available_bikes")    
-    private int availableBikes;
-
-    @SerializedName("bike_stands")
-    private int bikeStands;
-
-    private int number;
-
-    private String adress;
-
-    private String name;
-
-  //  private String bonus;
-  //  private String banking;
-    //private String contract_name;
-
+    private Integer availableBikeStands;
+    private Integer availableBikes;
+    private Integer number;
+    private Integer bikeStands;
     private String status;
+    private String name;
     private String address;
-    private double[] position;
-  //  private Date last_update;
+    private Double[] position;
 }
