8e21527299d6b66e137df078e85449f4b6a93b59
Maxime Cerjak <maxpro.cerjak@gmail.com>
2022-11-22 09:11:36 +0100

    tp dev mobile v0.4

--- src/main/java/com/formation/velo/api/parks/OpenDataParkingNantesClient.java
+++ src/main/java/com/formation/velo/api/parks/OpenDataParkingNantesClient.java
@@ -6,7 +6,7 @@ import retrofit2.http.GET;
 
 public interface OpenDataParkingNantesClient {
     
-    @GET("/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes-disponibilites&q=&facet=grp_nom&facet=grp_statut&rows=126")
+    @GET("/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes-disponibilites&q=&facet=grp_nom&facet=grp_statut")
     Call<OpenDataParkingNantes> getRecords();
 
 }
